6c2db438535d74eb126d9adf4c5ace570cf0c08a
conan <aconanlai@gmail.com>
2016-11-11 16:22:00 -0500

    configure for heroku

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 
 
 app.use(bodyParser.urlencoded({ extended: true, }));
@@ -11,21 +11,25 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 const url = 'mongodb://studio:studio@ds151117.mlab.com:51117/matricules';
 
-MongoClient.connect(url, (err, db) => {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-  }
+let db;
+
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('howison 3000');
+  });
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('nowison 3000');
+app.get('/documents', (req, res) => {
+  db.collection('documents').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    // renders index.ejs
+    res.send(result);
+  });
 });
-
-
 
