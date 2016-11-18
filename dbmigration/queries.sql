03fc40ab967f14d4a49ac2f3ba8995dca569c1c6
conan <aconanlai@gmail.com>
2016-11-18 15:49:41 -0500

    updated sql queries

--- dbmigration/queries.sql
+++ dbmigration/queries.sql
@@ -61,6 +61,16 @@ FROM term_node
 JOIN term_data ON term_node.tid=term_data.tid
 JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Keywords';
 
+
+/*
+create categorie map*/
+
+INSERT INTO categoriemap
+SELECT term_node.tid, term_node.vid as documentID,vocabulary.name as type, term_data.name
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Catégorie';
+
 /*
 get all keywords for each node
 */
@@ -95,10 +105,31 @@ AND node.type='content_document'
 return mediums
 */
 
-SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+SELECT term_node.tid, term_node.vid as documentID, vocabulary.name as type, term_data.name as medium
 FROM term_node
 JOIN term_data ON term_node.tid=term_data.tid
 JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium';
+
+/*
+return support and insert into map table
+*/
+
+INSERT into supportmap
+SELECT term_node.tid, term_node.vid as documentID, vocabulary.name as type, term_data.name as support
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Support';
+
+/*
+get links and format as json
+*/
+SELECT
+  vid,
+  GROUP_CONCAT(CONCAT('{url:"', field_lien_url, '", title:"',field_lien_title,'"}')) links
+FROM
+  content_field_lien
+GROUP BY
+  vid
 
 /*
 full query
@@ -112,4 +143,90 @@ JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentI
 FROM term_node
 JOIN term_data ON term_node.tid=term_data.tid
 JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+WHERE node.type='content_document'
+
+
+/*
+get subject
+
+use LEFT OUTER JOIN to include null values
+*/
+
+SELECT node.vid, content_type_content_document.field_title_value as formed_title, node.title as accession_number, keywordsmap.name as keywords, mediums.medium, content_type_content_document.field_date_value as date, content_field_sujet.field_sujet_value
+FROM node 
+LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
+JOIN content_type_content_document on content_type_content_document.vid=node.vid
+JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+LEFT OUTER JOIN content_field_sujet on content_field_sujet.vid=mediums.documentID
+WHERE node.type='content_document'
+
+/*
+add categories
+*/
+
+SELECT content_type_content_document.field_title_value as formed_title, node.title as accession_number, keywordsmap.name as keywords, mediums.medium, content_type_content_document.field_date_value as date, content_field_sujet.field_sujet_value, categoriemap.name as categorie
+FROM node 
+LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
+JOIN content_type_content_document on content_type_content_document.vid=node.vid
+JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+LEFT OUTER JOIN content_field_sujet on content_field_sujet.vid=mediums.documentID
+LEFT OUTER JOIN categoriemap on categoriemap.documentID=content_type_content_document.vid
+WHERE node.type='content_document'
+
+/*
+add support
+*/
+
+SELECT content_type_content_document.field_title_value as formed_title, node.title as accession_number, keywordsmap.name as keywords, mediums.medium, content_type_content_document.field_date_value as date, content_field_sujet.field_sujet_value, categoriemap.name as categorie, supportmap.name as support
+FROM node 
+LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
+JOIN content_type_content_document on content_type_content_document.vid=node.vid
+JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+LEFT OUTER JOIN content_field_sujet on content_field_sujet.vid=mediums.documentID
+LEFT OUTER JOIN categoriemap on categoriemap.documentID=content_type_content_document.vid
+LEFT OUTER JOIN supportmap on supportmap.documentID=content_type_content_document.vid
+WHERE node.type='content_document'
+
+/*
+with links
+*/
+SELECT content_type_content_document.field_title_value as formed_title, content_type_content_document.field_content_description_value, node.title as accession_number, keywordsmap.name as keywords, mediums.medium, content_type_content_document.field_date_value as date, content_field_sujet.field_sujet_value, categoriemap.name as categorie, supportmap.name as support, content_type_content_document.field_physical_description_value, linksmap.links as links
+FROM node 
+LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
+JOIN content_type_content_document on content_type_content_document.vid=node.vid
+JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+LEFT OUTER JOIN content_field_sujet on content_field_sujet.vid=mediums.documentID
+LEFT OUTER JOIN categoriemap on categoriemap.documentID=content_type_content_document.vid
+LEFT OUTER JOIN supportmap on supportmap.documentID=content_type_content_document.vid
+LEFT OUTER JOIN linksmap on linksmap.vid=content_type_content_document.vid
+WHERE node.type='content_document'
+
+/*
+with notes
+*/
+SELECT content_type_content_document.field_title_value as formed_title, content_type_content_document.field_content_description_value, node.title as accession_number, keywordsmap.name as keywords, mediums.medium, content_type_content_document.field_date_value as date, content_field_sujet.field_sujet_value, categoriemap.name as categorie, supportmap.name as support, content_type_content_document.field_physical_description_value, linksmap.links as links, notesmap.notes
+FROM node 
+LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
+JOIN content_type_content_document on content_type_content_document.vid=node.vid
+JOIN (SELECT term_node.tid, term_data.name as medium, term_node.vid as documentID, vocabulary.name as type
+FROM term_node
+JOIN term_data ON term_node.tid=term_data.tid
+JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
+LEFT OUTER JOIN content_field_sujet on content_field_sujet.vid=mediums.documentID
+LEFT OUTER JOIN categoriemap on categoriemap.documentID=content_type_content_document.vid
+LEFT OUTER JOIN supportmap on supportmap.documentID=content_type_content_document.vid
+LEFT OUTER JOIN linksmap on linksmap.vid=content_type_content_document.vid
+LEFT OUTER JOIN notesmap on notesmap.vid=content_type_content_document.vid
 WHERE node.type='content_document'
