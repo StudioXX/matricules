6ed50e8c1e3d6e46c5c5a4250197697e1362936f
conan <aconanlai@gmail.com>
2016-11-18 16:03:16 -0500

    new JSON

--- dbmigration/convertsql.js
+++ dbmigration/convertsql.js
@@ -22,15 +22,7 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
-const query = `SELECT node.vid,content_type_content_document.field_title_value as name,node.title,keywordsmap.name as keywords,mediums.medium,from_unixtime(content_type_content_document.field_date_value) as date
-FROM node 
-LEFT OUTER JOIN keywordsmap ON keywordsmap.documentID=node.vid
-JOIN content_type_content_document on content_type_content_document.vid=node.vid
-JOIN (SELECT term_node.tid,term_data.name as medium,term_node.vid as documentID,vocabulary.name as type
-FROM term_node
-JOIN term_data ON term_node.tid=term_data.tid
-JOIN vocabulary ON term_data.vid=vocabulary.vid and vocabulary.name='Médium') as mediums on mediums.documentID=node.vid
-WHERE node.type='content_document';`
+const query = `SELECT * FROM finalmap;`
 
 connection.query(query, function(err, rows, fields) {
   if (err) throw err;
