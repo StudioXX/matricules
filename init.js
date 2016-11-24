bef907c79e2fbd912fc8e178cf3360b30d260824
conan <aconanlai@gmail.com>
2016-11-24 16:57:01 -0500

    lol i'm intern

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 const server = require('./server/server').server;
 const config = require('./config/config');
 
+// listen
 server.listen(config.port, () => {
   console.log(`server started on port ${config.port} (${config.node_env})`);
 });
