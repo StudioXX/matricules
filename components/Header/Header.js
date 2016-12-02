af85a6a6c8a9556cb058dba2e1dca7403f7d5685
conan <aconanlai@gmail.com>
2016-12-02 17:25:52 -0500

    logout page

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -23,17 +23,17 @@ class Header extends React.Component {
   handleLogout() {
     console.log('logging out');
     unsetToken();
-    this.props.url.pushTo('/');
+    this.props.url.pushTo('/logout');
   }
+
   render() {
+    const logButton = this.props.loggedUser ? <button onClick={this.handleLogout} className={'button-primary'}>Logout</button> : <Link href="/login"><button className={'button-primary'}>Login</button></Link>;
     return (
       <div className={styles.menu}>
         <span className={styles.menuitem}><Link href="/">studio xx matricules app</Link></span>
         <span className={styles.menuitem}><Link href="/documents">documents list</Link></span>
         <span className={styles.menuitem}><Link href="/about">about</Link></span>
-
-        <Link href="/login"><button className={'button-primary'}>Login</button></Link>
-        <button onClick={this.handleLogout} className={'button-primary'}>Logout</button>
+        {logButton}
       </div>
     );
   }
---
+++ pages/logout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import defaultPage from '../HOC/DefaultPage';
+
+const Logout = () => (
+  <div>you are now logged out</div>
+);
+
+export default defaultPage(Logout);
